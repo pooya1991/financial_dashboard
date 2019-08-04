3487eb162f8691a56e5d2d0631b1506eff4e700d
mohsen mollayi <mmollayi@gmail.com>
2019-08-04 18:08:31 +0430

    a new working version of the app

--- globals.R
+++ globals.R
@@ -1,2 +1,41 @@
 source("R/ifrs_base.R")
 
+library(DT)
+dat <- iris[1:4]
+
+rowCallback_bs <- c(
+    "function(row, dat, displayNum, index){",
+    "  if(index == 0 || index == 4 || index == 5 || index == 10 || index == 11 || index == 12 || index == 14){",
+    "    for(var j=0; j<dat.length; j++){",
+    "      $('td:eq('+j+')', row)", 
+    "        .css('font-weight', 'bold')",
+    "    }",
+    "  }",
+    "  if(index == 4 || index == 10 || index == 11 || index == 14){",
+    "    for(var j=0; j<dat.length; j++){",
+    "      $('td:eq('+j+')', row)", 
+    "        .css('border-bottom', '1px solid black')",
+    "    }",
+    "  }",
+    "DTWidget.formatRound(this, row, dat, 1, 0, 3, ',', '.')",
+    "}"
+)
+
+rowCallback_is <- c(
+    "function(row, dat, displayNum, index){",
+    "  if(index == 2 || index == 9 || index == 10 || index == 11 || index == 13 || index == 14){",
+    "    for(var j=0; j<dat.length; j++){",
+    "      $('td:eq('+j+')', row)", 
+    "        .css('font-weight', 'bold')",
+    "    }",
+    "  }",
+    "  if(index == 1 || index == 2 || index == 8 || index == 9 || index == 10 || index == 13 || index == 14){",
+    "    for(var j=0; j<dat.length; j++){",
+    "      $('td:eq('+j+')', row)", 
+    "        .css('border-bottom', '1px solid black')",
+    "    }",
+    "  }",
+    "DTWidget.formatRound(this, row, dat, 1, 0, 3, ',', '.')",
+    "var value=dat[1]; $(this.api().cell(row, 1).node()).css({'color':isNaN(parseFloat(value)) ? '' : value <= -1e-5 ? 'red' : 'black'})",
+    "}"
+)
--- server.R
+++ server.R
@@ -28,12 +28,12 @@ shinyServer(function(input, output, session) {
             balance_sheet <- character(); bs_base <- double(); bs_premiums <- double()
         }
         
-        DT::datatable(tibble(balance_sheet, bs_base, bs_premiums),
-        colnames = c("Balance Sheet", "Base", "Premiums"), rownames = FALSE,
-        options = list(paging = FALSE, searching = FALSE)) %>% 
-            DT::formatRound(c(2, 3), digits = 0, mark = ",") %>% 
-            DT::formatStyle(c(2, 3), c(2, 3), target = "cell", 
-                            color = DT::styleInterval(-1e-5, c('red', 'black')))
+        DT::datatable(balance_sheet2,
+        colnames = c("Balance Sheet", "Base"), class = "strip",
+        options = list(paging = FALSE, searching = FALSE, rowCallback = JS(rowCallback_bs))) #%>% 
+            # DT::formatRound(c(2, 3), digits = 0, mark = ",") %>%
+            # DT::formatStyle(c(2, 3), c(2, 3), target = "cell",
+            #                 color = DT::styleInterval(-1e-5, c('red', 'black')))
         })
     
     output$income_statement <- DT::renderDT({
@@ -41,11 +41,11 @@ shinyServer(function(input, output, session) {
             income_statement <- character(); is_base <- double(); is_premiums <- double()
         }
         
-        DT::datatable(tibble(income_statement, is_base, is_premiums),
-        colnames = c("Income Statement", "Base", "Premiums"), rownames = FALSE,
-        options = list(paging = FALSE, searching = FALSE)) %>% 
-            DT::formatRound(c(2, 3), digits = 0, mark = ",") %>% 
-            DT::formatStyle(c(2, 3), c(2, 3), target = "cell", 
-                            color = DT::styleInterval(-1e-5, c('red', 'black')))
+        DT::datatable(income_statement2,
+        colnames = c("Income Statement", "Base"), class = "strip",
+        options = list(paging = FALSE, searching = FALSE, rowCallback = JS(rowCallback_is)))
+            # DT::formatRound(c(2, 3), digits = 0, mark = ",") %>% 
+            # DT::formatStyle(c(2, 3), c(2, 3), target = "cell", 
+            #                 color = DT::styleInterval(-1e-5, c('red', 'black')))
         })
 })
